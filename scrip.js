8407906fff8140e14e6c0c18f6216a2732b380e5
Kenjitian <lucas.kenji.cardoso@escola.pr.gov.br>
2025-05-30 12:57:43 -0300

    Desenvolvimento pt2

---
+++ scrip.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'Valorant',
+    'Quantos créditos um jogador recebe no início de uma partida em Valorant?',
+    '800 créditos.'
+)
+
+criaCartao(
+    'Valorant',
+    'Qual é o nome da arma mais cara do jogo?',
+    'Odin, custando 3.200 créditos.'
+)
+
+criaCartao(
+    'Valorant',
+    'Qual agente possui a habilidade de reviver um aliado?',
+    'Sage, com sua ultimate Ressurreição.'
+)
