3271da6345714064563e78458a9815047e6da91c
Kenjitian <lucas.kenji.cardoso@escola.pr.gov.br>
2025-05-30 14:38:57 -0300

    Conclusão do trabalho

---
+++ perguntas.js
@@ -0,0 +1,53 @@
+criaCartao(
+    'Valorant',
+    'Quantos créditos um jogador recebe no início de uma partida em Valorant?',
+    '800 créditos.'
+)
+
+criaCartao(
+    'Valorant',
+    'Qual é o nome da arma mais cara do jogo?',
+    'Odin, custando 3.200 créditos.'
+)
+
+criaCartao(
+    'Valorant',
+    'Qual agente possui a habilidade de reviver um aliado?',
+    'Sage, com sua ultimate Ressurreição.'
+)
+
+criaCartao(
+    'Valorant',
+    'Quantos rounds são necessários para vencer uma partida padrão de Valorant?',
+    '13 rounds, ou 2 rounds de diferença no overtime.'
+)
+
+criaCartao(
+    'Valorant',
+    'O que acontece quando o tempo da rodada acaba e a spike não foi plantada?',
+    'O time defensor vence o round.'
+)
+
+criaCartao(
+    'Valorant',
+    'Quantos segundos leva para a spike explodir após ser plantada?',
+    '40 segundos.'
+)
+
+criaCartao(
+    'Valorant',
+    'Qual o número máximo de jogadores em uma partida padrão de Valorant?',
+    '10 jogadores (5 contra 5).'
+)
+
+criaCartao(
+    'Valorant',
+    'Qual é o número máximo de créditos que um jogador pode acumular?',
+    '9.000 créditos.'
+)
+
+criaCartao(
+    'Valorant',
+    'Qual é a função principal de um controlador no Valorant?',
+    'Controlar áreas do mapa e bloquear a visão inimiga com smokes ou habilidades similares.'
+)
